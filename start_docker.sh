2a390dbb13081947d80731c9450890b2ece361c4
arthurgustin <arthur.gustin@gmail.com>
2018-02-27 23:02:06 +0100

    chore: delete useless script

--- start_docker.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run --name POSTGRES -v /home/agustin/gocode/src/github.com/DigitalFrameworksLLC/teddycare/sql/:/home/agustin/gocode/src/github.com/DigitalFrameworksLLC/teddycare/sql/ -d -p 5432:5432 -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=teddycare postgres
