1257ea79ef055bff45adf8421a80a941c9bf5453
arthurgustin <arthur.gustin@gmail.com>
2018-05-18 08:44:12 +0200

    chore: allow automatic deployment on kubernetes when commiting on master
    closes #23

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ ! -d "$HOME/google-cloud-sdk/bin" ]; then rm -rf $HOME/google-cloud-sdk; export CLOUDSDK_CORE_DISABLE_PROMPTS=1; curl https://sdk.cloud.google.com | bash; fi
+source /home/travis/google-cloud-sdk/path.bash.inc
+gcloud --quiet version
+gcloud --quiet components update
+echo $KUBERNETES_SERVICE_ACCOUNT > /tmp/kubernetes-service-account-key.json
+gcloud auth activate-service-account --key-file /tmp/kubernetes-service-account-key.json
+gcloud config set project $GCP_PROJECT
+gcloud container clusters get-credentials $KUBERNETES_CLUSTER_NAME --zone $KUBERNETES_CLUSTER_ZONE --project $GCP_PROJECT
+kubectl set image deployment/teddycare teddycare=eu.gcr.io/teddy-care/teddycare:$TRAVIS_COMMIT
